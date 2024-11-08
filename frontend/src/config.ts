6733bc3a72b98d76ab2476ba3927885485b5e4a5
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-11-08 17:22:33 +0100

    Fix API URL

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 // src/config.ts
 
 export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://l6xe0morxf.execute-api.eu-central-1.amazonaws.com/default/aws-coffee-management-system-backend-lambda'
+  ? 'https://l6xe0morxf.execute-api.eu-central-1.amazonaws.com/default/aws-coffee-management-system-backend-lambda/api'
   : 'http://localhost:5000/api';
