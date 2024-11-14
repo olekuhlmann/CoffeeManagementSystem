68b24ea8c1f58396e5e319b5a66a8576cc6f97e7
Ole Kuhlmann <tim.ole.kuhlmann@cern.ch>
2024-11-14 09:12:31 +0100

    Fix localhost URL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ const limiter = rateLimit({
 
 // Configure CORS to allow requests from your frontend origin and credentials
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+  origin: process.env.FRONTEND_URL || ['http://localhost:5173', 'http://127.0.0.1:5173'], 
   credentials: true,
 };
 
